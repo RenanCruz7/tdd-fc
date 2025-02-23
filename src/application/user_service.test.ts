01bfe36872bdbb4e905906fad22296a9d69e101b
RenanCruz7 <renancruz199@gmail.com>
2025-02-23 17:57:11 -0300

    Testando a funcao de salvar um usuario

--- src/application/user_service.test.ts
+++ src/application/user_service.test.ts
@@ -1,3 +1,4 @@
+import { User } from '../domain/entities/user';
 import { FakeUserRepository } from './../infrastructure/repositories/fake_user_repository';
 import { UserService } from './user_service';
 
@@ -22,6 +23,16 @@ describe('UserService', () => {
         expect(user).not.toBeNull();
         expect(user?.getId()).toBe('1');
         expect(user?.getName()).toBe('John Doe');
+    });
+
+    it("Deve salvar um novo usuario com sucesso usando repositorio fake e buscar ele novamente", async () => {
+        const newUser = new User('3', 'Renan Cruz');
+        await fakeUserRepository.save(newUser);
+
+        const user = await userService.findUserById('3');
+        expect(user).not.toBeNull();
+        expect(user?.getId()).toBe('3');
+        expect(user?.getName()).toBe('Renan Cruz');
     });
 
 });
