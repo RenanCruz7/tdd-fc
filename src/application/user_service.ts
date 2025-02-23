cb09cc641d06bd32c715a378dc71ec0724eef654
RenanCruz7 <renancruz199@gmail.com>
2025-02-23 17:52:52 -0300

    Desacoplando repository

--- src/application/user_service.ts
+++ src/application/user_service.ts
@@ -1,9 +1,11 @@
 import { User } from "../domain/entities/user";
+import { UserRepository } from "../domain/repositories/user_repository";
 import { FakeUserRepository } from "../infrastructure/repositories/fake_user_repository";
 
 export class UserService {
 
-    constructor(private readonly userRepository: FakeUserRepository) {}
+    // Injecting the repository and using them to get the user in the fake repository
+    constructor(private readonly userRepository: UserRepository) {}
 
     async findUserById(id: string): Promise<User | null>  {
         return this.userRepository.findById(id);
