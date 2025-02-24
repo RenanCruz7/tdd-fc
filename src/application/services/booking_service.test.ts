c92d26b7850d75a3068c9aaa0f829813a8057d10
RenanCruz7 <renancruz199@gmail.com>
2025-02-24 19:16:06 -0300

    Teste de createBooking e BookingService

--- src/application/services/booking_service.test.ts
+++ src/application/services/booking_service.test.ts
@@ -1,6 +1,67 @@
-describe('BookingService', () => {	
+import { FakeBookingRepository } from './../../infrastructure/repositories/fake_booking_repository';
+import { CreateBookingDTO } from './../dtos/create_booking_dto';
+import { Booking } from '../../domain/entities/booking';
+import { BookingService } from './booking_service';
+import { UserService } from './user_service';
+import { PropertyService } from './property_service';
 
-    it('Deve criar um booking com sucesso utilizando o repository fake', () => {
-        
+jest.mock('./property_service');
+jest.mock('./user_service');
+
+describe('BookingService', () => {	
+    let bookingService: BookingService;
+    let fakeBookingRepository: FakeBookingRepository;
+    let mockPropertyService: jest.Mocked<PropertyService>;
+    let mockUserService: jest.Mocked<UserService>;
+
+    beforeEach(() => {
+        const mockPropertyRepository = {} as any;
+        const mockUserRepository = {} as any;
+
+        mockPropertyService = new PropertyService(mockPropertyRepository) as jest.Mocked<PropertyService>;
+        mockUserService = new UserService(mockUserRepository) as jest.Mocked<UserService>;
+
+        fakeBookingRepository = new FakeBookingRepository();
+
+        bookingService = new BookingService(fakeBookingRepository, mockPropertyService, mockUserService);
+    });
+
+
+    it('Deve criar um booking com sucesso utilizando o repository fake', async () => {
+        // Mocks de property e user
+        const mockProperty = {
+            getId: jest.fn().mockReturnValue('1'),
+            isAvailable: jest.fn().mockReturnValue(true),
+            validateGuestCount: jest.fn(), // Corrigido aqui
+            calculateTotalPrice: jest.fn().mockReturnValue(500),
+            addBooking: jest.fn()
+        } as any;
+
+        const mockUser = {
+            getId: jest.fn().mockReturnValue('1')
+        } as any;
+
+        // Busca dos resultados de property e user
+        mockPropertyService.findPropertyById.mockResolvedValue(mockProperty);
+        mockUserService.findUserById.mockResolvedValue(mockUser);
+
+        const bookingDTO: CreateBookingDTO = {
+            propertyId: '1',
+            guestId: '1',
+            startDate: new Date('2021-10-10'),
+            endDate: new Date('2021-10-15'),
+            guestCount: 2
+        }
+
+        const result = await bookingService.createBooking(bookingDTO);
+
+        expect(result).toBeInstanceOf(Booking);
+        expect(result.getStatus()).toBe('CONFIRMED');
+        expect(result.getTotalPrice()).toBe(500);
+
+
+        const savedBooking = await fakeBookingRepository.findById(result.getId());  
+        expect(savedBooking).not.toBeNull();
+        expect(savedBooking?.getId()).toBe(result.getId());
     });
 });
---
+++ src/application/services/booking_service.ts
@@ -0,0 +1,53 @@
+import { DateRange } from './../../domain/value_objects/date_range';
+import { Booking } from "../../domain/entities/booking";
+import { BookingRepository } from "../../domain/repositories/booking_repository";
+import { CreateBookingDTO } from "../dtos/create_booking_dto";
+import { PropertyService } from "./property_service";
+import { UserService } from "./user_service";
+import { v4 as uuid } from 'uuid';
+
+export class BookingService {
+
+    constructor(
+        private readonly bookingRepository: BookingRepository,
+        private readonly propertyService: PropertyService,
+        private readonly userService: UserService
+    ){}
+
+
+    /**
+     * Ja havia feito o testo da propriedade e do usuario, agora preciso fazer o teste do booking
+     * utilizei os mocks para evitar a dependencia de outras classes e somente me concentrar na classe que estou testando que 
+     * é a BookingService
+     */
+
+
+
+    async createBooking(dto: CreateBookingDTO): Promise<Booking> {
+        const property = await this.propertyService.findPropertyById(dto.propertyId);
+
+       if(!property){
+        throw new Error('Propriedade não encontrada');
+       }
+
+       const guest = await this.userService.findUserById(dto.guestId);
+       if(!guest){
+        throw new Error('Usuario não encontrado');
+       }
+
+       const dateRange = new DateRange(dto.startDate, dto.endDate); // altamento acoplado precisa de mock para ser usado
+
+       const booking = new Booking(
+        uuid(),
+        property,
+        guest,
+        dateRange,
+        dto.guestCount
+       )
+
+       await this.bookingRepository.save(booking);
+       return booking;
+    }
+
+}
+   
